bbabcdf495167b7fbe576161b0817788878ceddc
wwmin <wwei.min@163.com>
2017-01-20 14:43:47 +0800

    中间件与错误处理

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ var app = express();
 var indexRouter = require('./routes/index');
 var userRouter = require('./routes/users');
 
+//中间件思 与 next
+app.use(function (req, res, next) {
+  console.log('0' + Date.now());
+  // next(new Error('我出错了...')); //到此就不会向下传递
+  next();
+});
+app.use(function (req, res, next) {
+  console.log('1');
+  // res.status(200).end();
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', userRouter);
@@ -15,4 +26,9 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+//错误处理
+app.use(function (err, req, res, next) {
+  console.log(err.stack);
+  res.status(500).send('出错了.')
+});
 app.listen(3000);
