ec182161432845b81a7a6b3c9f64a6c9650d0121
wwmin <wwei.min@163.com>
2017-01-20 14:17:16 +0800

    加入bodyparser 解析路径

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
-
+//body-parser 引用方式
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.get('/', function (req, res) {
   res.send('hello , express');
+});
+
+app.get('/users/:name', function (req, res) {
+  res.send('hello, ' + req.params.name);
+});
+
+app.get('/:url', function (req, res) {
+  console.log(JSON.stringify(req.body));
+  res.send(`你访问了:${req.params.url}`);
 });
 app.listen(3000);
